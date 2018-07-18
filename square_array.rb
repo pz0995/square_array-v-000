19cd533763128a21eab0cb1d066bcbf36a9079d3
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-07-18 04:53:59 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,12 +1,4 @@
-class Array
-  def square!
-    self.map! {|num| num ** 2}
-  end
-
-
-square_array.square!
 
 def square_array(array)
-  numbers = [1, 2, 3, 4, 5]
-end
+    Array.new(array.size) { |i| array[i] ** 2}
 end
